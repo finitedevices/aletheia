0c52ab97ae4b04fb954045fc5763d03bccbdee77
James-Livesey <jamesliveg@gmail.com>
2026-01-19 18:44:14 +0000

    Add basic simulator UI layout

--- simulator/script.js
+++ simulator/script.js
@@ -5,11 +5,35 @@ const SYSTEM_DIPS = [
     {name: "Custom B", class: "customB"}
 ];
 
+const INSTANCES = 4;
+
+function createInstanceApplyCheckbox(id) {
+    var container = document.createElement("div");
+    var checkbox = document.createElement("input");
+    var label = document.createElement("label");
+
+    checkbox.type = "checkbox";
+    checkbox.id = `applyInstance${id}`;
+    checkbox.checked = id == 0;
+
+    label.htmlFor = checkbox.id;
+    label.textContent = id;
+
+    container.append(checkbox);
+    container.append(label);
+
+    return container;
+}
+
 function createInstance(id) {
     var instance = document.createElement("div");
     var mode = "off";
 
+    instance.id = `instance${id}`;
     instance.className = "instance";
+    instance.hidden = id != 0;
+
+    instance.setAttribute("data-instanceid", id);
 
     instance.innerHTML = `
         <fieldset class="dips system">
@@ -85,6 +109,13 @@ function createInstance(id) {
     });
 
     document.querySelector("#instances").append(instance);
+    document.querySelector("#applyInstances").append(createInstanceApplyCheckbox(id));
+}
+
+function applyProperties(id) {
+    var instance = document.getElementById(`instance${id}`);
+
+    instance.hidden = !document.querySelector("#showInstance").checked;
 }
 
 Module.onRuntimeInitialized = function() {
@@ -92,11 +123,21 @@ Module.onRuntimeInitialized = function() {
 
     Module.setup();
 
-    createInstance(0);
+    for (var i = 0; i < INSTANCES; i++) {
+        createInstance(i);
+    }
+
+    document.querySelector("#applyButton").addEventListener("click", function() {
+        for (var i = 0; i < INSTANCES; i++) {
+            if (document.getElementById(`applyInstance${i}`).checked) {
+                applyProperties(i);
+            }
+        }
+    });
 
     requestAnimationFrame(function render() {
         Module.loop();
 
         requestAnimationFrame(render);
-    })
+    });
 };
